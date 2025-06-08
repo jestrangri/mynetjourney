ae62ed2009670a2b6ce2ade41de65ca1f9d7d5a9
jestrangri <jestrangrizz@gmail.com>
2025-06-08 09:25:39 +0800

    Add files via upload

---
+++ enabling video calls.ps1
@@ -0,0 +1,13 @@
+﻿@@@Enabling Video Calls
+configure terminal
+ephone 1
+   video
+   voice service voip
+   h323
+   call start slow
+ephone 2
+   video
+   voice service voip
+   h323
+   call start slow
+end
